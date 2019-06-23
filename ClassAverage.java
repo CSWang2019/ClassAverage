72d2b4792a1935dae2acdca663ab35d52ae2be88
CSWang2019 <51888135+CSWang2019@users.noreply.github.com>
2019-06-23 08:30:47 +0800

    Add files via upload

---
+++ ClassAverage.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class ClassAverage
+{
+	public static void main(String[] args)
+	{
+		int total = 0 ;
+		int gradeCounter = 0;
+		Scanner input = new Scanner(System.in);
+
+		System.out.print("Enter grade or -1 to quit:");
+		int grade = input.nextInt();
+
+
+		while (grade != -1)
+		{
+			total = total + grade;
+			gradeCounter = gradeCounter +1 ;
+			System.out.print("Enter grade or -1 to quit:");
+			grade = input.nextInt();
+			
+		}
+		if(gradeCounter != 0)
+		{
+			int average = total / gradeCounter;
+
+			System.out.printf("%nTotal of all 10 grades is %d%n", total);
+			System.out.printf("%nAverage of all 10 grades is %d%n", average);
+
+		}
+		else 
+			System.out.println("No grade were entered.");
+
+		
+
+
+
+	}
+}
